07e07180f59695cf07ecc3c4ab4b71d809072158
Katerina <bpbpqdqd@gmail.com>
2020-02-06 01:10:25 +0200

    refactor test command to sand email

--- src/MailBundle/Command/TestConsumerCommand.php
+++ src/MailBundle/Command/TestConsumerCommand.php
@@ -23,7 +23,38 @@ class TestConsumerCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->getContainer()->get('old_sound_rabbit_mq.send_email_producer')->publish('Ура, сообщенька...');
-        $this->getContainer()->get('old_sound_rabbit_mq.send_email_producer')->publish('bad');
+
+//        // Create the Transport
+//                $transport = (new \Swift_SmtpTransport('smtp.gmail.com', 587, 'tls'))
+//                    ->setUsername('bpbpqdqd@gmail.com')
+//                    ->setPassword(null)
+//            ;
+//
+//        // Create the Mailer using your created Transport
+//        $mailer = new \Swift_Mailer($transport);
+//
+//        // Create a message
+//        $message = (new \Swift_Message('News Letter Subscription'))
+//            ->setFrom(['app@example.com' => 'A Name'])
+//            ->setTo(['someone@example.com' => 'A Name'])
+//            ->setBody('your message body')
+//        ;
+//
+//        // Send the message
+//        $result = $mailer->send($message);
+        for ($i = 0; $i <= 5; $i++) {
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Some Subject')
+                ->setFrom('example@gmail.com')
+                ->setTo('any@whatever.com')
+                ->setBody('ExampleBundle:Default:mail.html.twig', 'text/html');
+
+            $mailer = $this->getContainer()->get('mailer');
+            $mailer->send($message);
+        }
+
+//        $this->getContainer()->get('old_sound_rabbit_mq.send_email_producer')->publish('Ура, сообщенька...');
+//        $this->getContainer()->get('old_sound_rabbit_mq.send_email_producer')->publish('bad');
+
     }
 }
